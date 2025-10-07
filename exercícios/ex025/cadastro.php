87b424d3fccb4a75ccf1bb4c84f33470cf9c5041
hepew <henrycrp19@gmail.com>
2025-10-07 11:38:52 -0300

    form002 e 003

--- exercícios/ex025/cadastro.php
+++ exercícios/ex025/cadastro.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 01 - Cadastro</title>
+</head>
+<body>
+    <h1>Cadastro Feito</h1>
 
-Todos os dados foram enviados! Obrigado
-Não esqueça de aprender PHP.
+</body>
+</html>
